8692c17af5dfb1bd3fd1582a1869beea7adeb871
alexjpujol <apujol@spotify.com>
2018-02-07 22:20:30 -0500

    updated var names

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ const paymentData = XLSX.readFile('input-data.csv');
 const contactData = XLSX.readFile('contact-data.xlsx');
 
 
-const campaigns = workbook["Sheets"]["Sheet1"];
-const jsonData = XLSX.utils.sheet_to_json(data);
+const campaigns = paymentData["Sheets"]["Sheet1"];
+const campaignJSON = XLSX.utils.sheet_to_json(campaigns);
 
 //filter out non-invoiced campaigns
-const invoicedRows = jsonData.filter(data => {
-    if (data.payment_description === 'Invoice') {
+const invoicedRows = campaignJSON.filter(data => {
+        if (data.payment_description === 'Invoice') {
+            return data;
+        }
+    })
+    .map(data => {
+        data.ad_name = "Insert ad name here";
+        //delete irrelevant properties?
         return data;
-    }
-})
+    });
 
-const plusAdName = invoicedRows.map(data => {
-    data.ad_name = "Insert add name here";
-    //delete irrelevant properties?
-    return data;
-})
 
-console.log(plusAdName);
+console.log(invoicedRows);
